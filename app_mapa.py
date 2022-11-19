d7eba748fa774f97565b9d3a0f41fb6cdb7985b1
Ángel González Muelas <84228711+DataAngelGonzalez@users.noreply.github.com>
2022-11-19 10:55:38 +0100

    Update app_mapa.py

--- app_mapa.py
+++ app_mapa.py
@@ -1,9 +1,22 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+import yfinance as yahooFinance
+import plotly.graph_objs as go
 
-df = pd.DataFrame(
-    np.random.randn(500, 2) / [1.2, 1.2] + [40.4165, -3.70256],
-    columns=['lat', 'lon'])
+# Selectores
+with st.sidebar:
+    cia = st.text_input(label = "Introduce compañía", value = 'AAPL')
+    interval = st.selectbox(label = "Intervalo a coger de la BD", options = ['1m','2m','5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo'])
+    period = st.selectbox(label = "Periodo", options = ['1d','5d','1mo','3mo','6mo','1y','2y','5y','10y','ytd','max'])
 
-st.map(df)
+# Datos
+tickets = yahooFinance.Ticker(cia)
+stocks = tickets.history(period = period, interval = interval).reset_index()
+
+if len(stocks) > 0:
+    fig = go.Figure()
+    fig.add_trace(go.Candlestick(x=stocks.Datetime, open = stocks.Open, high=stocks.High, low=stocks.Low, close=stocks.Close, name = 'market data'))
+    fig.update_layout(title = f'Stocks para {cia}', yaxis_title = 'Valor', width=1000, height=700)
+    st.plotly_chart(fig)
+    
+else:
+    st.info("El nombre de la compañía o la combinación 'Intervalo-Periodo' no se encuentra disponible en Yahoo Finance")
